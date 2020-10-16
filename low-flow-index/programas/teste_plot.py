b8af078efbeea3d4bf26c376669ee1020219d8aa
brunotjuliani <brunotjuliani@hotmail.com>
2020-10-16 11:19:50 -0300

    plot evento 2020

--- low-flow-index/programas/teste_plot.py
+++ low-flow-index/programas/teste_plot.py
@@ -8,8 +8,8 @@ import seaborn as sns
 import datetime as dt
 
 #DEFINICAO PERIODO ANALISE
-data_inicial = dt.datetime(2020, 1, 1,  0,  0) #YYYY, M, D, H, Min
-data_final = dt.datetime(2020, 10, 14,  23,  59)
+data_inicial = dt.datetime(2020, 1, 15,  0,  0) #YYYY, M, D, H, Min
+data_final = dt.datetime(2020, 6, 10,  23,  59)
 
 dir_plot = "/home/bruno/github/analise-seca/low-flow-index/dados-saida"
 os.chdir(dir_plot)
@@ -23,19 +23,43 @@ data_fim = data_final
 serie_observada = pd.read_csv(nome_bacia+'_serie.csv', index_col=0)
 serie_observada.index = pd.to_datetime(serie_observada.index)
 serie_observada = serie_observada.loc[str(data_ini) : str(data_fim)]
-serie_observada
+obs = serie_observada['q_m3s']
+q95 = serie_observada['q95']
 
 plt.figure()
 plt.plot(serie_observada['q_m3s'], label = "Observado", linewidth = 0.6, color = 'black')
-plt.plot(serie_observada['q95'], label = "Q95", linewidth = 0.5, color = 'red')
+plt.plot(serie_observada['q95'], label = "Q95", linewidth = 0.8, color = 'black')
+plt.hlines(y = 150, xmin = dt.datetime(2020,1,27), xmax=dt.datetime(2020,1,31), linewidth = 0.4)
+plt.text(y= 155, x = dt.datetime(2020,1,29), s = 'di', ha = 'center', weight = 'light')
+plt.hlines(y = 150, xmin = dt.datetime(2020,2,19), xmax=dt.datetime(2020,2,25), linewidth = 0.4)
+plt.text(y= 155, x = dt.datetime(2020,2,22), s = 'di', ha = 'center', weight = 'light')
+plt.hlines(y = 150, xmin = dt.datetime(2020,3,9), xmax=dt.datetime(2020,5,23), linewidth = 0.4)
+plt.text(y= 155, x = dt.datetime(2020,4,16), s = 'di', ha = 'center', weight = 'light')
+plt.hlines(y = 150, xmin = dt.datetime(2020,5,25), xmax=dt.datetime(2020,6,7), linewidth = 0.4)
+plt.text(y= 155, x = dt.datetime(2020,6,1), s = 'di', ha = 'center', weight = 'light')
+plt.scatter(y = [150, 150, 150, 150, 150, 150, 150, 150],
+            x = [dt.datetime(2020,1,27),dt.datetime(2020,1,31),
+                 dt.datetime(2020,2,19),dt.datetime(2020,2,25),
+                 dt.datetime(2020,3,9),dt.datetime(2020,5,23),
+                 dt.datetime(2020,5,25),dt.datetime(2020,6,7)],
+            marker = "|", linewidth = 0.4, color = 'black')
+plt.hlines(y = 80, xmin = dt.datetime(2020,2,1), xmax=dt.datetime(2020,2,19), linewidth = 0.4)
+plt.text(y= 85, x = dt.datetime(2020,2,10), s = 't < tc', ha = 'center', weight = 'light')
+plt.hlines(y = 80, xmin = dt.datetime(2020,2,26), xmax=dt.datetime(2020,3,9), linewidth = 0.4)
+plt.text(y= 85, x = dt.datetime(2020,3,3), s = 't < tc', ha = 'center', weight = 'light')
+plt.scatter(y = [80, 80, 80, 80],
+            x = [dt.datetime(2020,2,1),dt.datetime(2020,2,19),
+                 dt.datetime(2020,2,26),dt.datetime(2020,3,9)],
+            marker = "|", linewidth = 0.4, color = 'black')
 plt.xlabel('Data')
 plt.ylabel('Vazão [m3s-1]')
+plt.fill_between(serie_observada.index, obs, q95, where = (obs < q95), color = 'red', alpha = 0.3)
 # Format the date into months & days
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
 # Change the tick interval
 plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 # Puts x-axis labels on an angle
-plt.gca().xaxis.set_tick_params(rotation = 30)
+plt.gca().xaxis.set_tick_params(rotation = 0)
 # Changes x-axis range
 plt.gca().set_xbound(data_ini, data_fim)
 plt.savefig(nome_bacia+'_plot.png', dpi = 300)
