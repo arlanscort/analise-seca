fb7f2b890145b61aefe8b1caf1e0f08b7333cfd5
brunotjuliani <brunotjuliani@hotmail.com>
2021-02-16 16:09:10 -0300

    atualiza 16-02-21

--- low-flow-index/programas/teste_plot.py
+++ low-flow-index/programas/teste_plot.py
@@ -8,27 +8,31 @@ import seaborn as sns
 import datetime as dt
 
 #DEFINICAO PERIODO ANALISE
-data_inicial = dt.datetime(2020, 1, 1,  0,  0) #YYYY, M, D, H, Min
+#data_inicial = dt.datetime(2020, 7, 1,  0,  0) #YYYY, M, D, H, Min
+data_inicial = dt.datetime.now()- dt.timedelta(days=365)
 data_final = dt.datetime.now()
 
 dir_plot = "../dados-saida/"
 os.chdir(dir_plot)
 
 
-nome_bacia = 'uniao_da_vitoria'
+#nome_bacia = 'uniao_da_vitoria'
+nome_bacia = 'tomazina'
 
 data_ini = data_inicial
 data_fim = data_final
-
 serie_observada = pd.read_csv(nome_bacia+'_serie.csv', index_col=0)
 serie_observada.index = pd.to_datetime(serie_observada.index)
 serie_observada = serie_observada.loc[str(data_ini) : str(data_fim)]
 obs = serie_observada['q_m3s']
 q95 = serie_observada['q95']
+q50 = serie_observada['q50']
 
 plt.figure()
 plt.plot(serie_observada['q_m3s'], label = "Observado", linewidth = 0.6, color = 'black')
-plt.plot(serie_observada['q95'], label = "Q95", linewidth = 0.8, color = 'black')
+plt.plot(serie_observada['q95'], label = "Q95", linewidth = 0.8, color = 'maroon')
+plt.plot(serie_observada['q50'], label = "Q50", linewidth = 0.8, color = 'darkgoldenrod')
+plt.scatter(obs.index[-1], obs[-1], label = "Vazão atual", color = "darkblue", s = 15)
 '''
 plt.hlines(y = 150, xmin = dt.datetime(2020,1,27), xmax=dt.datetime(2020,1,31), linewidth = 0.4)
 plt.text(y= 155, x = dt.datetime(2020,1,29), s = 'di', ha = 'center', weight = 'light')
@@ -56,6 +60,7 @@ plt.scatter(y = [80, 80, 80, 80],
 plt.xlabel('Data')
 plt.ylabel('Vazão [m3s-1]')
 plt.fill_between(serie_observada.index, obs, q95, where = (obs < q95), color = 'red', alpha = 0.3)
+plt.fill_between(serie_observada.index, obs, q50, where = (obs < q50), color = 'gold', alpha = 0.3)
 # Format the date into months & days
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
 # Change the tick interval
@@ -64,5 +69,8 @@ plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 plt.gca().xaxis.set_tick_params(rotation = 30)
 # Changes x-axis range
 plt.gca().set_xbound(data_ini, data_fim)
+# Legenda
+plt.title(nome_bacia.replace('_',' ').title())
+plt.legend()
 plt.savefig(nome_bacia+'_plot2.png', dpi = 300)
 plt.show()
